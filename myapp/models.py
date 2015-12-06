dd4f79ace79f3738996677e49a943b58d70c49d9
xprtk <prtkagw@gmail.com>
2015-12-06 19:34:15 +0530

    Update models.py
    Add the models for neomodel

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,11 @@
 from django.db import models
-
+##prtk
+from neomodel import (StructuredNode, DateTimeProperty,BooleanProperty, StructuredRel ,StringProperty, IntegerProperty, RelationshipTo, RelationshipFrom)
+from uuid import uuid4
+import pytz
+from pytz import timezone
+from datetime import datetime, date
+##prtk
 # Create your models here.
 
 def generate_filename(self,filename):
@@ -47,3 +53,25 @@ class GetFriendRequests(models.Model):
     def __str__(self):
         return self.current_user
 
+#prtk
+class FriendRel(StructuredRel):
+    since = DateTimeProperty(default=lambda: datetime.now(pytz.utc))
+    met = StringProperty()
+
+class Post(StructuredNode):
+    post_id = StringProperty(unique_index=True, default=uuid4)
+    url=StringProperty(unique_index=True, default=uuid4)
+    desc=StringProperty()
+    owner=StringProperty(default="username@uuid4")#name of user
+
+class Posted(StructuredRel):
+    time = DateTimeProperty(default=lambda: datetime.now(pytz.utc))
+    location = StringProperty()
+    post_type = BooleanProperty()
+
+class Person(StructuredNode):
+    my_id=StringProperty(unique_index=True, default=uuid4)
+    name = StringProperty()
+    age = IntegerProperty(index=True, default=0)
+    friends = RelationshipTo('Person', 'is_friend', model=FriendRel)
+    posts = RelationshipTo('Post', 'post', model=Posted)
